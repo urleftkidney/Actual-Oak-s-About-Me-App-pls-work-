d0348ef770b2115f5b29fe03430d34cd02d10a27
sprklz <leila.kalinichenko7@gmail.com>
2023-08-11 10:57:11 -0400

    Initial Commit

---
+++ Actual Oak's About Me App (pls work)/Actual_Oak_s_About_Me_App__pls_work_App.swift
@@ -0,0 +1,17 @@
+//
+//  Actual_Oak_s_About_Me_App__pls_work_App.swift
+//  Actual Oak's About Me App (pls work)
+//
+//  Created by Len on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Actual_Oak_s_About_Me_App__pls_work_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Actual Oak's About Me App (pls work)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Actual Oak's About Me App (pls work)
+//
+//  Created by Len on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
